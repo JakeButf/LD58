97785b8851a9e9be0080fa3d5b8500c0e400d47c
Jake <jacobbutfiloski@gmail.com>
2025-10-03 19:36:05 -0400

    fix player controller jump

--- Assets/Scripts/Behaviors/Player/PlayerController.cs
+++ Assets/Scripts/Behaviors/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -9,15 +10,18 @@ public class PlayerController : MonoBehaviour
     [Header("Look Settings")]
     [SerializeField] private Camera camera;
     private float pitch = 0f;
-    
+
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 1.5f;
+
+    private Vector3 velocity;
 
     void Start()
     {
-        if(playerController == null) playerController = GetComponent<CharacterController>();
-        if(camera == null) camera = Camera.main;
+        if (playerController == null) playerController = GetComponent<CharacterController>();
+        if (camera == null) camera = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -29,15 +33,16 @@ public class PlayerController : MonoBehaviour
         //Debug Info
         if (SHOW_DEBUG)
         {
-            Debug.Log($"Player Position: {transform.position}");
-            Debug.Log($"Camera Position: {camera.transform.position}");
-            Debug.Log($"Player Rotation: {transform.rotation.eulerAngles}");
-            Debug.Log($"Camera Rotation: {camera.transform.rotation.eulerAngles}");
+            // Debug.Log($"Player Position: {transform.position}");
+            // Debug.Log($"Camera Position: {camera.transform.position}");
+            // Debug.Log($"Player Rotation: {transform.rotation.eulerAngles}");
+            // Debug.Log($"Camera Rotation: {camera.transform.rotation.eulerAngles}");
+            // Debug.Log($"Grounded? {playerController.isGrounded}");
         }
 
         //Camera
         float mouseX = Input.GetAxis("Mouse X") * PlayerInput.MouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * PlayerInput.MouseSensitivity; 
+        float mouseY = Input.GetAxis("Mouse Y") * PlayerInput.MouseSensitivity;
 
         pitch -= mouseY;
         pitch = Mathf.Clamp(pitch, -80f, 80f);
@@ -45,20 +50,31 @@ public class PlayerController : MonoBehaviour
         playerController.transform.Rotate(Vector3.up * mouseX);
         //Movement
         float speed = moveSpeed;
-        Vector3 velocity = Vector3.zero;
-        if (Input.GetKey(PlayerInput.MoveUp)) velocity += transform.forward;
-        if (Input.GetKey(PlayerInput.MoveDown)) velocity -= transform.forward;
-        if (Input.GetKey(PlayerInput.MoveLeft)) velocity -= transform.right;
-        if (Input.GetKey(PlayerInput.MoveRight)) velocity += transform.right;
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(PlayerInput.MoveUp)) move += transform.forward;
+        if (Input.GetKey(PlayerInput.MoveDown)) move -= transform.forward;
+        if (Input.GetKey(PlayerInput.MoveLeft)) move -= transform.right;
+        if (Input.GetKey(PlayerInput.MoveRight)) move += transform.right;
+        move = move.normalized * speed;
+        playerController.Move(move * Time.deltaTime);
 
-        playerController.Move(velocity.normalized * speed * Time.deltaTime);
-
-        if(playerController.isGrounded && velocity.y < 0)
+        if (IsGrounded() && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
+        if (IsGrounded()&& Input.GetKeyDown(PlayerInput.Jump))
+        {
+            Debug.Log("Jump");
+            velocity.y = Mathf.Sqrt(jumpHeight * gravity * -2f);
+        }
+
         velocity.y += gravity * Time.deltaTime;
         playerController.Move(velocity * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, playerController.height / 2f + 0.1f);
     }
 }
