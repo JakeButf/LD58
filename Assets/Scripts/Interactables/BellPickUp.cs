dd34b4e3624f50f5ebd1de7279d1f8a3a5567bf1
Jake <jacobbutfiloski@gmail.com>
2025-10-05 18:13:22 -0400

    add bell toll on pickup

--- Assets/Scripts/Interactables/BellPickUp.cs
+++ Assets/Scripts/Interactables/BellPickUp.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class BellPickup: PickUpInteractable
 {
     [SerializeField] private string flagToSet;
+    [SerializeField] private AudioSource bellSound;
     public override void PickUpSpecial()
     {
         GameFlags.SetFlag(flagToSet, true);
         GameState.Instance.AddBell();
+        bellSound.Play();
     }
 }
